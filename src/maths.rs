f84f7181fd969bdadc12dcc14ee0544c479eceeb
Michal Nazarewicz <mina86@mina86.com>
2021-04-27 14:17:53 +0200

    Restructure maths module

--- src/maths.rs
+++ src/maths.rs
@@ -1,89 +1,109 @@
+#[inline(always)]
 #[cfg(all(target_feature = "fma", not(test)))]
 pub(crate) fn mul_add(a: f32, b: f32, c: f32) -> f32 { a.mul_add(b, c) }
 
+#[inline(always)]
 #[cfg(any(not(target_feature = "fma"), test))]
 pub(crate) fn mul_add(a: f32, b: f32, c: f32) -> f32 { a * b + c }
 
 
+#[inline]
 pub(crate) fn dot_product(a: &[f32; 3], b: &[f32; 3]) -> f32 {
+    // SAFETY: We check with #[cfg] whether it’s safe to run those functions.
     #[cfg(target_feature = "sse4.1")]
-    return intel::dot_product_sse4_1(a, b);
+    return unsafe { sse::dot_product_sse4_1(a, b) };
     #[cfg(all(target_feature = "sse", not(target_feature = "sse4.1")))]
-    return intel::dot_product_sse(a, b);
+    return unsafe { sse::dot_product_sse(a, b) };
     #[cfg(not(target_feature = "sse"))]
-    return dot_product_base(a, b);
+    return dot_product_fallback(a, b);
 }
 
 
 #[allow(dead_code)]
-fn dot_product_base(a: &[f32; 3], b: &[f32; 3]) -> f32 {
+fn dot_product_fallback(a: &[f32; 3], b: &[f32; 3]) -> f32 {
     mul_add(a[2], b[2], mul_add(a[1], b[1], a[0] * b[0]))
 }
 
 
-#[test]
-fn test_dot_product() {
-    let a: [f32; 3] = [1.0, 2.0, 3.0];
-    let b: [f32; 3] = [2.0, 20.0, 200.0];
-    let want: f32 = 642.0;
-
-    assert_eq!(want, dot_product(&a, &b));
-    assert_eq!(want, dot_product_base(&a, &b));
-}
-
-
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-mod intel {
+#[cfg(any(target_feature = "sse", test))]
+mod sse {
     #[cfg(target_arch = "x86")]
     use ::core::arch::x86 as arch;
     #[cfg(target_arch = "x86_64")]
     use ::core::arch::x86_64 as arch;
 
-
-    #[cfg(any(target_feature = "sse", test))]
     #[allow(dead_code)]
-    fn m128_from_array(arr: &[f32; 3]) -> arch::__m128 {
+    #[target_feature(enable = "sse")]
+    unsafe fn m128_from_array(arr: &[f32; 3]) -> arch::__m128 {
         #[repr(C, align(16))]
         struct Arr([f32; 4]);
         let arr = Arr([arr[0], arr[1], arr[2], 0.0]);
-        unsafe { core::mem::transmute(arr) }
+        core::mem::transmute(arr)
     }
 
-    #[cfg(any(target_feature = "sse4.1", test))]
+    #[target_feature(enable = "sse4.1")]
     #[allow(dead_code)]
-    pub(super) fn dot_product_sse4_1(a: &[f32; 3], b: &[f32; 3]) -> f32 {
+    pub(super) unsafe fn dot_product_sse4_1(a: &[f32; 3], b: &[f32; 3]) -> f32 {
         let a = m128_from_array(a);
         let b = m128_from_array(b);
-        unsafe { arch::_mm_cvtss_f32(arch::_mm_dp_ps(a, b, 0b1111_0001)) }
+        arch::_mm_cvtss_f32(arch::_mm_dp_ps(a, b, 0b1111_0001))
     }
 
-    #[cfg(any(target_feature = "sse", test))]
+    #[target_feature(enable = "sse")]
     #[allow(dead_code)]
-    pub(super) fn dot_product_sse(a: &[f32; 3], b: &[f32; 3]) -> f32 {
+    pub(super) unsafe fn dot_product_sse(a: &[f32; 3], b: &[f32; 3]) -> f32 {
         let a = m128_from_array(a);
         let b = m128_from_array(b);
-        unsafe {
-            let v = arch::_mm_mul_ps(a, b);
-            // https://stackoverflow.com/questions/6996764/fastest-way-to-do-horizontal-sse-vector-sum-or-other-reduction/35270026#35270026
-            let shuf = arch::_mm_shuffle_ps(v, v, 0b10_11_00_01);
-            let sums = arch::_mm_add_ps(v, shuf);
-            let shuf = arch::_mm_movehl_ps(shuf, sums);
-            let sums = arch::_mm_add_ss(sums, shuf);
-            arch::_mm_cvtss_f32(sums)
-        }
+        let v = arch::_mm_mul_ps(a, b);
+        // https://stackoverflow.com/questions/6996764/fastest-way-to-do-horizontal-sse-vector-sum-or-other-reduction/35270026#35270026
+        let shuf = arch::_mm_shuffle_ps(v, v, 0b10_11_00_01);
+        let sums = arch::_mm_add_ps(v, shuf);
+        let shuf = arch::_mm_movehl_ps(shuf, sums);
+        let sums = arch::_mm_add_ss(sums, shuf);
+        arch::_mm_cvtss_f32(sums)
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    const A: [f32; 3] = [1.0, 2.0, 3.0];
+    const B: [f32; 3] = [2.0, 20.0, 200.0];
+    const WANT: f32 = 642.0;
+
+    #[test]
+    pub fn test_dot_product() {
+        assert_eq!(WANT, super::dot_product(&A, &B));
+        assert_eq!(WANT, super::dot_product_fallback(&A, &B));
+    }
+
+    fn unsupported(requirement: &str) {
+        panic!(
+            "{} required to run this test.  This failure does not mean the \
+             implementation is incorrect; just that we’re unable to test it.",
+            requirement
+        );
     }
 
     #[test]
-    fn test_dot_product() {
-        let a: [f32; 3] = [1.0, 2.0, 3.0];
-        let b: [f32; 3] = [2.0, 20.0, 200.0];
-        let want: f32 = 642.0;
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+    fn test_mul_matrix_sse() { unsupported("x86 or x86_64 CPU"); }
 
-        if std::is_x86_feature_detected!("sse4.1") {
-            assert_eq!(want, dot_product_sse(&a, &b));
+    #[test]
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    fn test_mul_matrix_sse() {
+        if is_x86_feature_detected!("sse") {
+            unsafe {
+                assert_eq!(WANT, super::sse::dot_product_sse(&A, &B));
+            }
         }
-        if std::is_x86_feature_detected!("sse4.1") {
-            assert_eq!(want, dot_product_sse4_1(&a, &b));
+        if is_x86_feature_detected!("sse4.1") {
+            unsafe {
+                assert_eq!(WANT, super::sse::dot_product_sse4_1(&A, &B));
+            }
+        } else {
+            unsupported("SSE 4.1 support");
         }
     }
 }
