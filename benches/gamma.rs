0b46c3601964e9fb1344ad5aa5a07351db4136a2
Michal Nazarewicz <mina86@mina86.com>
2021-05-11 00:48:51 +0200

    Add benchmarks for Rec.709 gamma functions

--- benches/gamma.rs
+++ benches/gamma.rs
@@ -22,6 +22,50 @@ fn compress_u8(c: &mut criterion::Criterion) {
     });
 }
 
+fn expand_rec709_8bit(c: &mut criterion::Criterion) {
+    c.bench_function("expand 8-bit Rec.709", move |b| {
+        b.iter(|| {
+            for e in 16..=235 {
+                criterion::black_box(srgb::gamma::expand_rec709_8bit(e));
+            }
+        });
+    });
+}
+
+fn compress_rec709_8bit(c: &mut criterion::Criterion) {
+    c.bench_function("compress 8-bit Rec.709", move |b| {
+        b.iter(|| {
+            for s in 0..=219 {
+                criterion::black_box(srgb::gamma::compress_rec709_8bit(
+                    s as f32 / 219.0,
+                ));
+            }
+        });
+    });
+}
+
+fn expand_rec709_10bit(c: &mut criterion::Criterion) {
+    c.bench_function("expand 10-bit Rec.709", move |b| {
+        b.iter(|| {
+            for e in 64..=940 {
+                criterion::black_box(srgb::gamma::expand_rec709_10bit(e));
+            }
+        });
+    });
+}
+
+fn compress_rec709_10bit(c: &mut criterion::Criterion) {
+    c.bench_function("compress 10-bit Rec.709", move |b| {
+        b.iter(|| {
+            for s in 0..=876 {
+                criterion::black_box(srgb::gamma::compress_rec709_10bit(
+                    s as f32 / 876.0,
+                ));
+            }
+        });
+    });
+}
+
 fn expand_normalised(c: &mut criterion::Criterion) {
     c.bench_function("expand normalised", move |b| {
         b.iter(|| {
@@ -50,6 +94,10 @@ criterion_group!(
     benches,
     expand_u8,
     compress_u8,
+    expand_rec709_8bit,
+    compress_rec709_8bit,
+    expand_rec709_10bit,
+    compress_rec709_10bit,
     expand_normalised,
     compress_normalised,
 );
