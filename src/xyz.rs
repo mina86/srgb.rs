fe2b17b4687075f4eb40271b9faf7fc2306a652d
Michal Nazarewicz <mina86@mina86.com>
2021-06-08 17:54:42 +0200

    Add copyright header to each source file

--- src/xyz.rs
+++ src/xyz.rs
@@ -1,3 +1,18 @@
+/* This file is part of srgb crate.
+ * Copyright 2021 by Michał Nazarewicz <mina86@mina86.com>
+ *
+ * srgb crate is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * srgb crate is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * srgb crate.  If not, see <http://www.gnu.org/licenses/>. */
+
 //! Functions and constant handling and related to conversion between linear
 //! sRGB space and CIE XYZ colour space.
 
